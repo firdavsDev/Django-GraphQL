f3e76c001e9e1c39bc47518be9cfea61f331686c
firdavsDev <tarjimatv1@gmail.com>
2022-07-01 17:44:15 +0500

    add new futeres and undustand resolve_ func

--- books/schema.py
+++ books/schema.py
@@ -12,7 +12,7 @@ class Query(graphene.ObjectType):
 
     all_books = graphene.List(BooksType)
 
-    def resolve_all_books(root, info):
-        return Books.objects.filter(title="django")
+    def resolve_all_books(self, info):
+        return Books.objects.all()
 
 schema = graphene.Schema(query=Query)
